e77f7c27b77e8abec2e47e40bb10003bbea17e13
Baptiste Noleau <bnoleau@adlere.fr>
2024-04-10 11:01:33 +0200

    personalize url ServiceInfos

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import { type Plugin, ServiceInfos } from '@cpn-console/hooks'
 import monitor from './monitor.js'
 import { deleteProjectOpenProject, upsertProjectOpenProject } from './functions.js'
 import { readFileSync } from 'fs'
+import { join } from 'path'
 
-const imageData = Buffer.from((readFileSync('./files/logo.png')).toString('base64'))
+const imageData = Buffer.from((readFileSync(join(__dirname, '/files/logo.png'))).toString('base64'))
 
 const infos: ServiceInfos = {
   name: 'openproject',
-  to: () => 'https://support.dev.numerique-interieur.com/',
+  to: ({ project, organization }) => `https://support.dev.numerique-interieur.com/projects/${organization}-${project}/work_packages`,
   title: 'Support',
   imgSrc: `data:image/png;base64,${imageData}`,
   description: 'Outil de ticketing pour contacter la Service Team',
